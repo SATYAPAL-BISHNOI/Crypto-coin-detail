7f3ff10d11e19d1b57236ec31a53544df8a07d4f
satayapal <satyapalbishnoi@outlook.com>
2024-06-27 23:41:14 +0530

    Create all components file

--- cryptoProject/src/App.jsx
+++ cryptoProject/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
 
-
+import {BrowserRouter as Router , Route, Routes} from 'react-router-dom'
+import Home from './components/Home'
+import Header from './components/Header'
+import Coins from './components/Coins'
+import Exchanges from './components/Exchanges'
+import CoinDetails from './components/CoinDetails'
 
 
 function App() {
 
   return (
-    <>
-     
-    </>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/coins' element={<Coins/>}/>
+      <Route path='/Exchange' element={<Exchanges/>}/>
+      <Route path='/CoinDetails' element={<CoinDetails/>}/>
+    </Routes>
+   </Router>
   )
 }
 
